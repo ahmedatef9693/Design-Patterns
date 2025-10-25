3a220370a5220bb798cf43f7b27fd62f8fc27aff
ahmed atef <99121164+ahmedatef9693@users.noreply.github.com>
2025-10-25 22:24:06 +0300

    Add files via upload

---
+++ Singleton/singleton_flag.py
@@ -0,0 +1,19 @@
+
+
+
+class Signleton:
+    _instances = {}
+
+    def __new__(cls,*args,**kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__new__(cls)
+            cls._instances[cls].initialized = False
+        return cls._instances[cls]
+    def __init__(self):
+        if not self.initialized:
+            print("loading daata")
+            self.initialized = True
+
+
+a = Signleton()
+b = Signleton()
