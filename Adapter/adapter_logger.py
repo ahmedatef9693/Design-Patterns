4e910fc08aaccd64e21cf8e920ace51e6fc85dfd
ahmed atef <99121164+ahmedatef9693@users.noreply.github.com>
2025-10-30 02:17:43 +0300

    Add files via upload

---
+++ Adapter/adapter_logger.py
@@ -0,0 +1,26 @@
+class Logger:
+    def __init__(self, file_name=None):
+        self.file_name = file_name
+
+    def message(self, msg):
+        print(msg)
+
+class AdapterLogger:
+    index = 0
+    def __init__(self, logger: Logger, file_name="temp.txt"):
+        self.logger = logger
+        self.file_name = file_name
+
+    def message(self, msg):
+
+        with open(self.file_name, "a") as f:
+            f.write(f"{self.index} : {msg} " +'\n')
+        self.index+=1
+
+
+log = Logger()
+adapter = AdapterLogger(log, "output.txt")
+adapter.message("Hello")
+    
+
+ 
