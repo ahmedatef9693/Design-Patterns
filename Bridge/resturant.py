4a6be4f8e6575dfe004138821b2a7250f51c5ab2
ahmed atef <99121164+ahmedatef9693@users.noreply.github.com>
2025-11-05 23:50:10 +0200

    Add files via upload

---
+++ Bridge/resturant.py
@@ -0,0 +1,40 @@
+class Resturnat:
+    def __init__(self,pizza:Pizza):
+        self.pizza = pizza
+
+        
+    def make(self):
+        pass
+
+class AmericanResturant(Resturnat):
+    def __init__(sel,pizza:Pizza):
+        super().__init__(pizza)
+        
+    def make(self):
+        print(f"Making {self.pizza.__class__.__name__} in American Resturant")
+
+
+class ItalianResturant(Resturnat):
+    def __init__(self,pizza:Pizza):
+        super().__init__(pizza)
+        
+    def make(self):
+        print(f"Making {self.pizza.__class__.__name__} in Italian Resturant")
+
+class Pizza:
+    def __init__(self):
+        pass
+class CheesePizza(Pizza):
+    def __init__(self):
+        pass
+
+class PepperoniPizza(Pizza):
+    def __init__(self):
+        pass
+
+
+italian_resturant = ItalianResturant(PepperoniPizza())
+american_resturant = AmericanResturant(CheesePizza())
+
+italian_resturant.make()
+american_resturant.make()
